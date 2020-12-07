c8fd9703a0bb6e4dd85d0a1115d4d2739361dd86
KauanBrites <briteskauan@gmail.com>
2020-12-07 15:20:07 -0300

    Att Register

--- register.js
+++ register.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
 navigator.serviceWorker.register('sw.js')
   .then(function(){console.log('serviceWorker está registrado')})
-  .cash(function(){console.log('Erro, serviceWorker não está registrado')})
+  .catch(function(){console.log('Erro, serviceWorker não está registrado')})
 }
